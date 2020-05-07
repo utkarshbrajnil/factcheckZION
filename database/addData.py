575d6420f395b3fb09f1840926da20d3ee0075bd
Malay Rai <rai.malay09@gmail.com>
2020-05-07 23:52:04 +0530

    Example for adding data

---
+++ database/addData.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import json
+from pymongo import MongoClient
+
+client=MongoClient('localhost',27017)
+db=client['ZION']
+coll=db['user_info']
+
+with open('C:/Users/MR7/Desktop/user.json') as f:
+    data=json.load(f)
+
+coll.insert_many(data)
+
+client.close()
+
+print("completed")
