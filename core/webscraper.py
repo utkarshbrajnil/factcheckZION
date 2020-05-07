c97e675cbb6c0b3c22c6c6230f939d0e1e5e0bc6
Utkarsh Brajnil <brajnilutkarsh@gmail.com>
2020-05-07 22:39:37 +0530

    p tag

--- core/webscraper.py
+++ core/webscraper.py
@@ -13,9 +13,20 @@ soup = BeautifulSoup(google_search.text, 'html.parser')
 
 search_results = soup.select('.kCrYT a')
 #print(search_results)
+results=[]
 
 for link in search_results[:5]:
-    print(link.get('href'))
+    actual_link=link.get('href')
+    link_result=requests.get("https://google.com/"  + actual_link)
+    soup=BeautifulSoup(link_result.text, 'html.parser')
+    a=soup.select('p')
+    results.append(a)
+
+for r in results:
+    print(r)
+    print("\n")
+
+
 
 '''in the above code all the webpage links are stored in the list search_results
 in order of their appearance in the result of google search. By using the for loop
