5c8d8adf31d9e1ece0755344a76252a891270cf0
Utkarsh Brajnil <brajnilutkarsh@gmail.com>
2020-05-08 23:52:48 +0530

    return link

--- data processing/app.py
+++ data processing/app.py
@@ -1,5 +1,9 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+import requests, webbrowser
+import bs4 as bs
+from bs4 import BeautifulSoup
+import urllib.request
 
 app = Flask(__name__)
 
@@ -12,10 +16,13 @@ def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
     msg = request.form.get('Body')
-
+    google_search = requests.get("https://www.google.com/search?q="+msg+"factcheck")
+    soup = BeautifulSoup(google_search.text, 'html.parser')
+    search_results = soup.select('.kCrYT a')
+    msgr=search_results[0]
     # Create reply
     resp = MessagingResponse()
-    resp.message(msg)
+    resp.message(msgr)
    
 
     return str(resp)
